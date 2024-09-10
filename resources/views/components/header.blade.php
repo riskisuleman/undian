f0838db02dee838fbb3d777ecba19a1128b2b617
riskisuleman <sulemanriski33@gmail.com>
2024-09-10 23:30:32 +0800

    editing header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,16 +1,18 @@
 <header id="header" class="header fixed-top" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between">
         <a href="index-2.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
-            <img src="assets/img/jtxlogo.png" alt="">
+            <span style="font-weight: bold; font-size: 24px; color:blueviolet">Dapoer Dindra</span>
         </a>
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto" href="index-2.html">Home</a></li>
-                <li><a class="nav-link scrollto" href="index-3.html#about">Tentang Kami</a></li>
-                <li><a class="nav-link scrollto" href="index-3.html#contact">Hubungi Kami</a></li>
+                <li><a class="nav-link scrollto" href="index-2.html">Dashboard</a></li>
+                <li><a class="nav-link scrollto" href="index-2.html">Data Peserta</a></li>
+                <li><a class="nav-link scrollto" href="index-3.html#about">Undian</a></li>
+                <li><a class="nav-link scrollto" href="index-3.html#contact">Pemenang Undian</a></li>
+                <li><a class="nav-link scrollto" href="index-3.html#contact">Hadiah Undian</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle d-none"></i>
         </nav>
-        <a class="btn-getstarted scrollto" href="index-3.html#about">Tentang JTX</a>
+        <a class="btn-getstarted scrollto" href="index-3.html#about" style="background-color: blueviolet">Logout</a>
     </div>
 </header>
