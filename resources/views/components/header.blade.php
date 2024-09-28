c561d651f2fcf17f7c6a826fc055398cbfd57348
icaltayabu <icaltayabu@gmail.com>
2024-09-28 10:26:24 +0800

    editing views

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="header fixed-top" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between">
         <a href="index-2.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
-            <span style="font-weight: bold; font-size: 24px; color:rgb(16, 141, 204)">Dapoer Dindra</span>
+            <span style="font-weight: bold; font-size: 24px; color:#a333ff">Dapoer Dindra</span>
         </a>
         <nav id="navbar" class="navbar">
             <ul>
@@ -10,13 +10,14 @@
                 <li><a class="nav-link scrollto" href="{{ route('pemenang_undian.index') }}">Pemenang Undian</a>
                 </li>
                 <li><a class="nav-link scrollto" href="#">Hadiah Undian</a></li>
-                <li><a class="nav-link scrollto" href="#">Pengaturan</a></li>
-      
+
             </ul>
             <i class="bi bi-list mobile-nav-toggle d-none"></i>
         </nav>
 
-        <a class="btn-getstarted scrollto" href="{{ route('logout') }}"
-            style="background-color:rgb(16, 141, 204)">Logout</a>
+        <a class="btn-getstarted scrollto" href="{{ route('logout') }}" style="background-color:#a333ff; color:white;">
+            <i class="fas fa-sign-out-alt"></i> Logout
+        </a>
+
     </div>
 </header>
--- resources/views/pages/pages_home/index.blade.php
+++ resources/views/pages/pages_home/index.blade.php
@@ -16,12 +16,12 @@
                 <div class="col-md-4 mt-4 pt-2">
                     <h3 class="mb-4">00212</h3>
                     <div class="tombol-container">
-                        <button class="btn btn-primary mb-4">Start</button>
-                        <button class="btn btn-primary mb-4">Stop</button>
+                        <button class="btn mb-4" style="background-color: #a333ff; color:white">Start</button>
+                        <button class="btn mb-4" style="background-color: #a333ff; color:white">Stop</button>
                     </div>
 
-                    <button class="btn btn-primary mb-4">Nama Peserta</button><br>
-                    <button class="btn btn-primary mb-4">Simpan</button>
+                    <button class="btn mb-4" style="background-color: #a333ff; color:white">Nama Peserta</button><br>
+                    <button class="btn mb-4" style="background-color: #a333ff; color:white">Simpan</button>
                 </div>
 
                 <div class="col-md-4 mt-4 pt-2">
