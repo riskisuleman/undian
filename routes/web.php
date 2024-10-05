25f4ccd2b4d0b9c3ab8dfc184f8543377116bc68
icaltayabu <icaltayabu@gmail.com>
2024-10-05 14:43:38 +0800

    route akses

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PemenangController;
 use App\Http\Controllers\PemenangUndianController;
 use App\Http\Controllers\UndianController;
-use App\Models\Pemenang;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 
 Route::get('/', function () {
@@ -34,18 +22,11 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('home', [HomeController::class, 'index'])->name('home');
-
-Route::resource('data_peserta', DataPesertaController::class);
-Route::resource('pemenang_undian', PemenangController::class);
-Route::resource('hadiah_undian', HadiahUndianController::class);
-
-// Rute untuk undian
-Route::get('/undian', [UndianController::class, 'index'])->name('undian.index');
-
-// Rute untuk menyimpan undian
-Route::post('/undian/simpan', [HomeController::class, 'simpanUndian'])->name('undian.simpan');
-
-
-
-
+Route::middleware('auth')->group(function () {
+    Route::get('home', [HomeController::class, 'index'])->name('home');
+    Route::resource('data_peserta', DataPesertaController::class);
+    Route::resource('pemenang_undian', PemenangController::class);
+    Route::resource('hadiah_undian', HadiahUndianController::class);
+    Route::get('/undian', [UndianController::class, 'index'])->name('undian.index');
+    Route::post('/undian/simpan', [HomeController::class, 'simpanUndian'])->name('undian.simpan');
+});
